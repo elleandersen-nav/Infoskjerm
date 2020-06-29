a845b9f01dd6b259817cf90068ab73f3d8c85da4
Ellen Marie Andersen <ellen.marie.andersen@nav.no>
2020-06-29 14:18:47 +0200

    add clock

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Reddit from "./Reddit";
 import Bikes from "./Bikes";
 import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
       <h1>Infoskjerm</h1>
+      <Clock />
       <Reddit />
       <Bikes />
     </div>

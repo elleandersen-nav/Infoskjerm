253ccc8e102e9d655b80c02a329691d09806114a
Ellen Marie Andersen <ellen.marie.andersen@nav.no>
2020-06-29 13:42:49 +0200

    Updates

---
+++ precache-manifest.dccd00705dcd757261afc246edd2fc85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6241f32de5f75d33e10f93498b019a",
+    "url": "/Infoskjerm/index.html"
+  },
+  {
+    "revision": "e80d148eb53586eccb3f",
+    "url": "/Infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2925c3c7b84a53e8679a",
+    "url": "/Infoskjerm/static/js/2.d14d3afd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Infoskjerm/static/js/2.d14d3afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80d148eb53586eccb3f",
+    "url": "/Infoskjerm/static/js/main.6e282cc2.chunk.js"
+  },
+  {
+    "revision": "ff7c7fb72a57287e703d",
+    "url": "/Infoskjerm/static/js/runtime-main.4fa9607a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Infoskjerm/static/media/logo.5d5d9eef.svg"
+  }
+]);
